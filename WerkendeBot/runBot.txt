9f2b4c69a17d556ad3cda2b36fba28e9fd649819
Jordy <123079515+JorrdyCode@users.noreply.github.com>
2024-04-30 13:05:29 +0200

    Add files via upload

---
+++ WerkendeBot/runBot.txt
@@ -0,0 +1,3 @@
+@echo off
+
+python C:\Users\EVGA3080\Documents\NHA\GameBot Python\WerkendeBot\main.py
---
+++ enemy_template.py
@@ -0,0 +1,13 @@
+
+goblinFamily = {
+    "Goblin": {
+        "Level": 1,
+        "Weapon": "Copper Dagger",
+        "Attack": 2,
+        "Strength": 7,
+        "Agility": 8,
+        "Intelligence": 2,
+        "Wisdom": 0,
+        "Luck": 10
+    }
+}
